f8c674df2f31c13ace665018aab61d97854743bd
idtek <dishiwei@gmail.com>
2020-04-20 15:23:35 +0800

    up

--- android/src/main/java/com/silbermond/tencent_cos/TencentCosPlugin.java
+++ android/src/main/java/com/silbermond/tencent_cos/TencentCosPlugin.java
@@ -27,7 +27,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** TencentCosPlugin */
 public class TencentCosPlugin implements FlutterPlugin, MethodCallHandler {
   Registrar registrar;
-  MethodChannel channel;
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "tencent_cos");
@@ -46,80 +46,81 @@ public class TencentCosPlugin implements FlutterPlugin, MethodCallHandler {
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "tencent_cos");
 
-    this.registrar = registrar;
-    this.channel = channel;
+    registrar = registrar;
+
     channel.setMethodCallHandler(new TencentCosPlugin());
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+  public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("TencentCos.uploadFile")) {
-          LocalSessionCredentialProvider localCredentialProvider = new LocalSessionCredentialProvider(call.<String>argument("secretId"),
-                  call.<String>argument("secretKey"), call.<String>argument("sessionToken"),
-                  Long.parseLong(call.argument("expiredTime").toString())
-          );
-          String region = call.argument("region");
-          String appid = call.argument("appid");
-          String bucket = call.argument("bucket");
-          String cosPath = call.argument("cosPath");
-          final String localPath = call.argument("localPath");
-          TransferConfig transferConfig = new TransferConfig.Builder().build();
-          CosXmlServiceConfig.Builder builder = new CosXmlServiceConfig.Builder().setAppidAndRegion(appid, region).setDebuggable(false).isHttps(true);
-//创建 CosXmlServiceConfig 对象，根据需要修改默认的配置参数
-          CosXmlServiceConfig serviceConfig = new CosXmlServiceConfig(builder);
-          CosXmlService cosXmlService = new CosXmlService(registrar.context(), serviceConfig, localCredentialProvider);
-          //初始化 TransferManager
-          TransferManager transferManager = new TransferManager(cosXmlService, transferConfig);
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "tencent_cos");
+        LocalSessionCredentialProvider localCredentialProvider = new LocalSessionCredentialProvider(call.<String>argument("secretId"),
+                call.<String>argument("secretKey"), call.<String>argument("sessionToken"),
+                Long.parseLong(call.argument("expiredTime").toString())
+        );
+        String region = call.argument("region");
+        String appid = call.argument("appid");
+        String bucket = call.argument("bucket");
+        String cosPath = call.argument("cosPath");
+        final String localPath = call.argument("localPath");
+        TransferConfig transferConfig = new TransferConfig.Builder().build();
+        CosXmlServiceConfig.Builder builder = new CosXmlServiceConfig.Builder().setAppidAndRegion(appid, region).setDebuggable(false).isHttps(true);
+    //创建 CosXmlServiceConfig 对象，根据需要修改默认的配置参数
+        CosXmlServiceConfig serviceConfig = new CosXmlServiceConfig(builder);
+        CosXmlService cosXmlService = new CosXmlService(registrar.context(), serviceConfig, localCredentialProvider);
+        //初始化 TransferManager
+        TransferManager transferManager = new TransferManager(cosXmlService, transferConfig);
 
-//上传文件
-          COSXMLUploadTask cosxmlUploadTask = transferManager.upload(bucket, cosPath, localPath, null);
+    //上传文件
+        COSXMLUploadTask cosxmlUploadTask = transferManager.upload(bucket, cosPath, localPath, null);
 
-          final HashMap<String, Object> data = new HashMap<>();
-          data.put("localPath", localPath);
-          data.put("cosPath", cosPath);
-          cosxmlUploadTask.setCosXmlProgressListener(new CosXmlProgressListener() {
+        final HashMap<String, Object> data = new HashMap<>();
+        data.put("localPath", localPath);
+        data.put("cosPath", cosPath);
+        cosxmlUploadTask.setCosXmlProgressListener(new CosXmlProgressListener() {
 
-              @Override
-              public void onProgress(long complete, long target) {
-                  ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
-                      @Override
-                      public void run() {
-                          channel.invokeMethod("onProgress", data);
-                      }
-                  });
+            @Override
+            public void onProgress(long complete, long target) {
+                ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        channel.invokeMethod("onProgress", data);
+                    }
+                });
 
-                  Log.e("TencentCosPlugin", "onProgress =${progress * 100.0 / max}%");
-              }
-          });
-          //设置返回结果回调
-          cosxmlUploadTask.setCosXmlResultListener(new CosXmlResultListener() {
-              @Override
-              public void onSuccess(CosXmlRequest request, CosXmlResult httPesult) {
-                  Log.d("TencentCosPlugin", "Success: " + httPesult.printResult());
-                  ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
-                      @Override
-                      public void run() {
-                          result.success(data);
-                      }
-                  });
-              }
+                Log.e("TencentCosPlugin", "onProgress =${progress * 100.0 / max}%");
+            }
+        });
+        //设置返回结果回调
+        cosxmlUploadTask.setCosXmlResultListener(new CosXmlResultListener() {
+            @Override
+            public void onSuccess(CosXmlRequest request, CosXmlResult httPesult) {
+                Log.d("TencentCosPlugin", "Success: " + httPesult.printResult());
+                ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.success(data);
+                    }
+                });
+            }
 
-              @Override
-              public void onFail(CosXmlRequest request, CosXmlClientException exception, CosXmlServiceException serviceException) {
-                  Log.d("TencentCosPlugin", "Failed: " + (exception.toString() + serviceException.toString()));
-                  data.put("message", (exception.toString() + serviceException.toString()));
-                  ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
-                      @Override
-                      public void run() {
-                          result.error("失败了", "失败了", "失败了");
-                      }
-                  });
+            @Override
+            public void onFail(CosXmlRequest request, CosXmlClientException exception, CosXmlServiceException serviceException) {
+                Log.d("TencentCosPlugin", "Failed: " + (exception.toString() + serviceException.toString()));
+                data.put("message", (exception.toString() + serviceException.toString()));
+                ((Activity) registrar.activeContext()).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.error("failed", "failed", "failed");
+                    }
+                });
 
-              }
-          });
-      } else {
-          result.notImplemented();
-      }
+            }
+        });
+    } else {
+        result.notImplemented();
+    }
   }
 
   @Override
